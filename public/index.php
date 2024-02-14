3db30dea6a810af69c9030ef6dcd010836410dee
JuKohl <26julienkohler@gmail.com>
2024-02-14 11:15:53 +0100

    save

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\HttpFoundation\Request;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
@@ -8,5 +9,10 @@ return function (array $context) {
   return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 // echo 'ok ça marche'.$context['APP_ENV'].$context['APP_DEBUG'];
 };
-
+$trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false;
+$trustedProxies = $trustedProxies ? explode(',', $trustedProxies) : [];
+if($_SERVER['APP_ENV'] == 'prod') $trustedProxies[] = $_SERVER['REMOTE_ADDR'];
+if($trustedProxies) {
+    Request::setTrustedProxies($trustedProxies, Request::HEADER_X_FORWARDED_AWS_ELB);
+}
 // echo 'ok ça marche'.$context['APP_ENV'].$context['APP_DEBUG'];
