68ec097b8859268025ae9013cdf558f366e1c62d
JuKohl <26julienkohler@gmail.com>
2024-02-14 13:25:03 +0100

    save

--- public/index.php
+++ public/index.php
@@ -6,28 +6,11 @@ use Symfony\Component\HttpFoundation\Response;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-$kernel = function (array $context) {
+return function (array $context) {
   return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 // echo 'ok ça marche'.$context['APP_ENV'].$context['APP_DEBUG'];
 };
 
-// $kernelInstance = $kernel($_SERVER);
-
-// function configureTrustedProxies() {
-//   $trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false;
-//   $trustedProxies = $trustedProxies ? explode(',', $trustedProxies) : [];
-//   if($_SERVER['APP_ENV'] == 'prod') $trustedProxies[] = $_SERVER['REMOTE_ADDR'];
-//   if($trustedProxies) {
-//       Request::setTrustedProxies($trustedProxies, Request::HEADER_X_FORWARDED_AWS_ELB);
-//   }
-// }
-
-// configureTrustedProxies();
-
-// $request = Request::createFromGlobals();
-
-// $response = $kernelInstance->handle($request);
-
-// $response->send();
-
-// $kernelInstance->terminate($request, $response);
+if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
+  Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
+};
