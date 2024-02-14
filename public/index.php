e28e4ef7d7b11b89b11166418e00dd6a93eb8a3f
JuKohl <26julienkohler@gmail.com>
2024-02-14 12:09:21 +0100

    save

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-return function (array $context) {
+$kernel = function (array $context) {
   return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 // echo 'ok ça marche'.$context['APP_ENV'].$context['APP_DEBUG'];
 };
