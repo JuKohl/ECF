82709a91b01373ab25a1f2b5fe8d746eda5e10ea
JuKohl <26julienkohler@gmail.com>
2024-02-14 13:27:26 +0100

    save

--- public/index.php
+++ public/index.php
@@ -11,6 +11,9 @@ return function (array $context) {
 // echo 'ok ça marche'.$context['APP_ENV'].$context['APP_DEBUG'];
 };
 
-if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
-  Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
+$trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false;
+$trustedProxies = $trustedProxies ? explode(',', $trustedProxies) : [];
+if($_SERVER['APP_ENV'] == 'prod') $trustedProxies[] = $_SERVER['REMOTE_ADDR'];
+if($trustedProxies) {
+    Request::setTrustedProxies($trustedProxies, Request::HEADER_X_FORWARDED_AWS_ELB);
 };
