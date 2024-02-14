517df799bfa65cec83eae130edeab5784b5816aa
JuKohl <26julienkohler@gmail.com>
2024-02-14 17:14:58 +0100

    save

--- public/index.php
+++ public/index.php
@@ -5,6 +5,6 @@ use App\Kernel;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
   
 return function (array $context) {
-  echo 'ça marche' . $context['APP_ENV'] . $context['APP_DEBUG'];
-    // return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+  // echo 'ça marche' . $context['APP_ENV'] . $context['APP_DEBUG'];
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
