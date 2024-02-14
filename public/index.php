fefd97416bd8a1218cda936f773ef6be45a5efd3
JuKohl <26julienkohler@gmail.com>
2024-02-14 14:31:46 +0100

    save

--- public/index.php
+++ public/index.php
@@ -14,7 +14,7 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
   // };
   
   return function (array $context) {
-    echo 'ok ça marche'.$context['APP_ENV'].$context['APP_DEBUG'];
-  //   return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    // echo 'ok ça marche'.$context['APP_ENV'].$context['APP_DEBUG'];
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
   };
   
